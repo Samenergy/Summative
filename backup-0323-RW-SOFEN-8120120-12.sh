5c95a57a86c4273e5ca757aad54f8e159b1df82a
Ayen-Guech <a.guech@alustudent.com>
2023-06-18 12:48:12 -0700

    backup

--- backup-0323-RW-SOFEN-8120120-12.sh
+++ backup-0323-RW-SOFEN-8120120-12.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash 
-source_move-to-directory"/path/to/source/move-to-directory"
-destination_move-to-directory="/path/to/remote/server/move-to-directory"
-remote_server="user@remote-server"
-backup_file="backup-0323-RW-SOFEN-8120120-12.sh.tar.gz"
-tar -czf "$backup_file" "$source_move-to-directory"
-scp "$backup_file" "$remote_server:$destination_move-to-directory"
+#!/usr/bin/env bash
+source_directory="move-to-directory"
+destination_directory="/home/sftp-student/03033/summative"
+remote_server="a0dfc11ba139@a0dfc11ba139.a982e858.alu-cod.online"
+backup_file="backup-0323-RW-SOFEN-8120120-12.sh"
+tar -czf "$backup_file" "$source_directory"
+sshpass -p "899c79fc532cd0591af1" scp "$backup_file" "$remote_server:$destination_directory"
 rm "$backup_file"
-echo "Backup completed successfully!"
+echo "backup done"
